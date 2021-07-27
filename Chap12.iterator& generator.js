e9f164d95df9c42e42aeb93df2d1c3e466934f23
ariake <kyh31876@gmail.com>
2021-07-27 16:35:16 +0900

    'main'

--- Chap12.iterator& generator.js
+++ Chap12.iterator& generator.js
@@ -1,3 +1,49 @@
+//summary 
+
+
+//The for/of loop and the ... spread operator work with iterable objects.
+
+//An object is iterable if it has a method with the symbolic name
+//[Symbol.iterator] that returns an iterator object.
+
+//An iterator object has a next() method that returns an iteration result object.
+
+/* An iteration result object has a value property that holds the
+next iterated value, if there is one. If the iteration has
+completed, then the result object must have a done property
+set to true. */
+
+
+/*  You can implement your own iterable objects by defining a
+[Symbol.iterator]() method that returns an object
+with a next() method that returns iteration result objects.
+You can also implement functions that accept iterator
+arguments and return iterator values. */
+
+
+
+/* Generator functions (functions defined with function*
+instead of function) are another way to define iterators. */ 
+
+ 
+/* When you invoke a generator function, the body of the
+function does not run right away; instead, the return value is
+an iterable iterator object. Each time the next() method of
+the iterator is called, another chunk of the generator function
+runs. */ 
+
+
+/* Generator functions can use the yield operator to specify the
+values that are returned by the iterator. Each call to next()
+causes the generator function to run up to the next yield
+expression. The value of that yield expression then becomes
+the value returned by the iterator. When there are no more
+yield expressions, then the generator function returns, and
+the iteration is complete. */
+
+
+
+
 //Arrays (including TypedArrays) are iterable, 
 //as are strings and Set and Map objects.
 
@@ -294,3 +340,257 @@ let o = {
     }
 };
 [...o.g()] // => ["x", "y", "z", "g"]
+//Note that there is no way to write a generator function using arrow
+//function syntax.
+
+//Generators often make it particularly easy to define iterable classes.
+
+//We can replace the [Symbol.iterator]() method show in Example 12-1 
+//with a much shorter * [Symbol.iterator&rbrack;() generator function that looks
+//like this:
+
+*[Symbol.iterator](){
+    for(let x = Math.cil(this.from); x <= this.to; x++)
+yeild x;
+}
+
+
+
+//12.3.1 Generator Examples
+
+//for example, is a generator function that yields Fibonacci numbers:
+function* fibonacciSequence() {
+    let x = 0, y = 1;
+    for(;;) {
+        yield y;
+    [x, y] = [y, x+y]; // Note: destructuring assignment
+    }
+}
+//Note that the fibonacciSequence() generator function here has
+//an infinite loop and yields values forever without returning.
+
+//If this generator is used with the ... spread operator, it will loop until
+//memory is exhausted and the program crashes.
+
+
+//With care, it is possible to use it in a "for/of" loop, howerver:
+// Return the nth Fibonacci number
+function fibonacci(n) {
+    for(let f of fibonacciSequence()) {
+        if (n-- <= 0) return f;
+    }
+}
+fibonacci(20) // => 10946
+
+
+
+//This kind of infinite generator becomes more useful with a take()
+//generator like this:
+
+// Yield the first n elements of the specified iterable object
+function* take(n, iterable) {
+    let it = iterable[Symbol.iterator](); // Get iterator for iterable object
+        while(n-- > 0) { // Loop n times:
+            let next = it.next(); // Get the next item from the iterator.
+            if (next.done) return; // If there are no more values, return early
+            else yield next.value; // otherwise, yield the value
+    }
+}
+// An array of the first 5 Fibonacci numbers
+[...take(5, fibonacciSequence())] // => [1, 1, 2, 3, 5]
+
+
+//Here is another useful generator function that interleaves the elements
+//of multiple iterable objects:
+
+// Given an array of iterables, yield their elements in interleaved order.
+function* zip(...iterables) {
+// Get an iterator for each iterable
+    let iterators = iterables.map(i => i[Symbol.iterator]());
+    let index = 0;
+    while(iterators.length > 0) { // While there are still some iterators
+        if (index >= iterators.length) { // If we reached the last iterator
+            index = 0; // go back to the first one.
+        }
+        let item = iterators[index].next(); // Get next item from next iterator.
+            if (item.done) { // If that iterator is done
+                iterators.splice(index, 1); // then remove it from the array.
+            }
+        else { // Otherwise,
+            yield item.value; // yield the iterated value
+            index++; // and move on to the next iterator.
+        }
+    }
+}
+// Interleave three iterable objects
+[...zip(oneDigitPrimes(),"ab",[0])] // =>
+[2,"a",0,3,"b",5,7]
+
+
+
+//12.3.2 yield* and Recursive Generators
+
+/*In addition to the zip() generator defined in the preceding example,
+it might be useful to have a similar generator function that yields the
+elements of multiple iterable objects sequentially rather than
+interleaving them. */ 
+
+function* sequence(...iterables){
+    for(let iterable of iterables){
+        for(let item of iterable){
+            yield item;
+        }
+    }
+}
+
+[...sequence("abc",oneDigitPrimes())] // => ["a","b","c",2,3,5,7]
+
+
+
+//The yield* keyword is like yield except that, rather than
+//yielding a single value, it iterates an iterable object and yields each of
+//the resulting values.
+
+//The sequence() generator function that we’ve used can be simplified with yield* 
+//like this:
+
+function* sequence(...iterables) { 
+    for(let iterable of iterables){
+        yeild* iterable;
+    }
+}
+[...sequence("abc",oneDigitPrimes())] // => ["a","b","c",2,3,5,7]
+
+
+//The array forEach() method is often an elegant way to loop over
+//the elements of an array, so you might be tempted to write the
+//sequence() function like this:
+
+
+function* sequence(...iterables){
+    iterables.forEach(iterable => yeild* iterable); // Error
+}
+//yield and yield* can only be used within generator functions, but
+//the nested arrow function in this code is a regular function 
+//so yield is not allowed.
+
+//This means that yield* allows us to define recursive generators, 
+//and you might use this feature to allow simple non-recursive iteration 
+//over a recursively defined tree structure,
+
+
+
+
+//12.4.1 The Return Value of a Generator Function
+
+//The generator functions we’ve seen so far have not had return statement,
+
+//Like any function, though, a generator function can return a value.
+
+//The return value of the next() function is an object that 
+//has a value property and/or a done property.
+
+
+function *oneAndDone() {
+    yield 1;
+    return "done";
+}
+// The return value does not appear in normal iteration.
+[...oneAndDone()] // => [1]
+// But it is available if you explicitly call next()
+let generator = oneAndDone();
+generator.next() // => { value: 1, done: false}
+generator.next() // => { value: "done", done: true
+} // If the generator is already done, the return value is not returned again
+generator.next() // => { value: undefined, done: true }
+
+//With typical iterators and generators, if the value property
+//is defined, then the done property is undefined or is false. And if
+//done is true, then value is undefined.
+
+// But in the case of a generator that returns a value, 
+//the final call to "next" returns an object that has both "value" and "done" defined.
+
+/*The "value" property holds the return value of the generator function, 
+and the "done" property is true, 
+indicating that there are no more values to iterate. */
+
+/*  This final value is ignored by the for/of loop and by the spread operator, 
+but it is available to code that manually iterates with explicit calls to next(): 
+*/
+
+
+
+
+
+
+//12.4.2 The Value of a yield Expression
+
+
+/* The generator and caller are two separate streams of
+execution passing values (and control) back and forth. The following
+code illustrates: */ 
+function* smallNumbers() {
+    console.log("next() invoked the first time; argument discarded");
+    let y1 = yield 1; // y1 == "b"
+    console.log("next() invoked a second time with argument",
+y1);
+    let y2 = yield 2; // y2 == "c"
+    console.log("next() invoked a third time with argument",
+y2);
+    let y3 = yield 3; // y3 == "d"
+    console.log("next() invoked a fourth time with argument",
+y3);
+    return 4;
+    }
+let g = smallNumbers();
+console.log("generator created; no code runs yet");
+let n1 = g.next("a"); // n1.value == 1
+console.log("generator yielded", n1.value);
+let n2 = g.next("b"); // n2.value == 2
+console.log("generator yielded", n2.value);
+let n3 = g.next("c"); // n3.value == 3
+console.log("generator yielded", n3.value);
+let n4 = g.next("d"); // n4 == { value: 4, done: true }
+console.log("generator returned", n4.value);
+
+//When the next() method of a generator is invoked, the generator
+//function runs until it reaches a yield expression.
+
+//the expression that folllows the "yeild" keyword is evaluatedm and that value 
+//become the return value of the next() invocation. 
+
+//At this point, the generator function stops executing right in the middle 
+//of evaluating the yield expression.
+
+//The next time the next() method of the generator is called,
+//the argument passed to next() becomes the value of the 
+//yield expression that was paused.
+
+
+
+
+//When this code runs, it produces the following output that
+//demonstrates the back-and-forth between the two blocks of code:
+
+/*generator created; no code runs yet
+next() invoked the first time; argument discarded
+generator yielded 1
+next() invoked a second time with argument b
+generator yielded 2
+next() invoked a third time with argument c
+generator yielded 3
+next() invoked a fourth time with argument d
+generator returned 4 */
+
+// The first invocation of next() starts the generator,
+//but the value passed to that invocation is not accessible to the generator 
+
+
+
+//12.4.4 A Final Note About Generators 
+
+//They give us the ability to pause a computation with yield and 
+//restart it again at some arbitrary later time with an arbitrary input value.
+
+
